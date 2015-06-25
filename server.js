503ebb21102dba9048118a1e425d15d711a352f5
Daniel Schulz-Jackson <daniel@10x.agency>
2015-06-24 21:35:51 -0500

    basic REST API for organizations. number property isn't saving...

---
+++ server.js
@@ -0,0 +1,79 @@
+
+// Modules =========================
+var express = require('express');
+var app 	= express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// Configuration ===================
+
+mongoose.connect('mongodb://localhost/gp-tut');     // connect to mongoDB database on modulus.io // PERHAPS THIS IS A SANDBOXED THING?
+
+// the middleware stack
+app.use(express.static(__dirname + '/public')); // set static dir to public
+app.use(morgan('dev')); // log all requests
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+// listen (start app with node server.js)
+app.listen(8080);
+console.log("App listening on port 8080");
+
+
+// http://mongoosejs.com/docs/guide.html
+var organizationSchema = mongoose.Schema({
+    name: String,
+	portion : Number
+});
+
+var Organization = mongoose.model('Organization', organizationSchema);
+
+
+
+// Routes =============================
+
+app.get('/api/organizations', function(req,res){
+	Organization.find(function(err, organizations){
+		if(err)
+			res.send(err);
+
+		res.json(organizations);
+	})
+});
+
+app.post('/api/organizations', function(req,res){
+	Organization.create({
+		name : req.body.name,
+		number : req.body.portion
+	}, function(err,organizations){ // why plural?
+		if(err)
+			res.send(err);
+
+		Organization.find(function(err,organizations){
+			if(err)
+				res.send(err);
+
+			res.json(organizations);
+		})
+	}) 
+});
+
+app.delete('/api/organizations/:organization_id', function(req,res){
+	Organization.remove({
+		_id : req.params.organization_id
+	}, function(err, organizations){
+		if(err)
+			res.send(err);
+
+		Organization.find(function(err,organizations){
+			if(err)
+				res.send(err);
+
+			res.json(organizations);
+		})
+	})
+});
